f3eb05860f96311678b0a89f55f47ffcd4989361
Doness <doness@users.noreply.github.com>
2019-07-09 17:43:36 +0700

    Update Config Deploy

--- admin/include/db_config.php
+++ admin/include/db_config.php
@@ -1,7 +1,7 @@
 <?php
-    define('DB_SERVER','localhost');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD','');
-    define('DB_DATABASE','wedding');
+    define('DB_SERVER','ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define('DB_USERNAME','p08586meziz3nbih');
+    define('DB_PASSWORD','u2zpuft0hukjuczf');
+    define('DB_DATABASE','wae84v27hr0ovwh2');
 
 ?>
